a8f7a1fa915a6c00ff7f8d1a0bef258628548348
Jefferson Rodrigues A. Sousa <jefferson.sousa@interfile.com.br>
2017-04-20 18:05:26 -0300

    Commit Inicial

---
+++ BuscaImagens/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace BuscaImagens
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textDiretorio = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnProcurar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.txtdestino = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textDiretorio
+            // 
+            this.textDiretorio.Location = new System.Drawing.Point(88, 198);
+            this.textDiretorio.Name = "textDiretorio";
+            this.textDiretorio.Size = new System.Drawing.Size(506, 20);
+            this.textDiretorio.TabIndex = 0;
+            this.textDiretorio.TextChanged += new System.EventHandler(this.textDiretorio_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 201);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Diretorio: ";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // btnProcurar
+            // 
+            this.btnProcurar.Location = new System.Drawing.Point(501, 319);
+            this.btnProcurar.Name = "btnProcurar";
+            this.btnProcurar.Size = new System.Drawing.Size(75, 23);
+            this.btnProcurar.TabIndex = 2;
+            this.btnProcurar.Text = "Procurar";
+            this.btnProcurar.UseVisualStyleBackColor = true;
+            this.btnProcurar.Click += new System.EventHandler(this.btnProcurar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(582, 319);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 23);
+            this.btnSair.TabIndex = 3;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // txtdestino
+            // 
+            this.txtdestino.Location = new System.Drawing.Point(88, 234);
+            this.txtdestino.Name = "txtdestino";
+            this.txtdestino.Size = new System.Drawing.Size(506, 20);
+            this.txtdestino.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 237);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Diretorio: ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 365);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtdestino);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnProcurar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textDiretorio);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textDiretorio;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnProcurar;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.TextBox txtdestino;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ BuscaImagens/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace BuscaImagens
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void btnProcurar_Click(object sender, EventArgs e)
+        {
+
+            Listar();
+
+        }
+        private void LerArquivo()
+        {
+            System.IO.StreamReader file = new System.IO.StreamReader(@"c:\temp\test.txt");
+
+
+        }
+
+        private void Listar()
+        {
+            int counter = 0;
+            string line;
+
+            System.IO.StreamReader file =
+                                          new System.IO.StreamReader(@"c:\temp\teste.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                DirectoryInfo Dir = new DirectoryInfo(@"C:\temp\teste");
+                // Busca automaticamente todos os arquivos em todos os subdiretórios
+                FileInfo[] Files = Dir.GetFiles("*", SearchOption.AllDirectories);
+                foreach (FileInfo File in Files)
+                {
+                    // Retira o diretório iformado inicialmente
+                    string fileName = File.FullName;
+                    FileInfo fi = new FileInfo(fileName);
+                    var name = fi.Name;
+                    var dir = fi.DirectoryName;
+                    var full = fi.FullName;
+                   
+                    FileInfo filine = new FileInfo(line);
+                    var nameline = filine.Name;
+                    var dirline = filine.DirectoryName;
+                    var fullline = filine.FullName;
+
+                    if (name == nameline)
+                    {
+
+                        System.IO.File.Move(fileName, line);
+
+
+                    }
+                    else
+                    {
+                        Listar();
+                    }
+
+                }
+            }
+            counter++;
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        public void textDiretorio_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
